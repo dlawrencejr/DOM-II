494ec7398621816bf3df083d8bb3678d0132e72b
Dereck Lawrence Jr <d.lawrencejr@icloud.com>
2019-06-11 13:27:00 -0400

    started

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+let navMouse = document.querySelector('.nav');
+// console.log(navMouse);
+navMouse.addEventListener('mouseover', (event) => {
+    event.target.style.color = "blue";
+    setTimeout(function() {
+        event.target.style.color = "";
+      }, 200);
+    }, false);
+
+
